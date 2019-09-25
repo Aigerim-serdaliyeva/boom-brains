cdc33380ab07465db7570001f01a5a5c191ecf2c
Айгерим Сердалиева <aigerim.serdaliyeva@mail.ru>
2019-09-25 18:21:06 +0600

    first commit

---
+++ app/js/localization.js
@@ -0,0 +1,118 @@
+const messages = {
+
+  en: {   
+
+    header: {
+      title1: 'UI/UX Design',
+      title2: 'Web services development',
+      title3: 'Development of mobile games and applications'
+    },
+
+    menu: {
+      all: 'All projects',
+      game: 'Games',
+      app: 'Applications',
+      service: 'Web-service',
+      site: 'Websites'
+    },
+
+    work: {
+      textMurPart: 'Corporate website for a law firm',
+      textbbh: 'Site for a residential complex of townhouses in the mountains of Almaty',
+      textProfRem: 'Website for a professional repair center',
+      textProfLegal: 'Website for the Chamber of Legal Advisers',
+      textTSTrack: 'Mobile panic button site',
+      textTS: 'Website for a security company',
+      textTopSignal: 'Landing for the application mobile button',
+      textNewEstate: 'Corporate website of a construction company',
+      textTSKZ: 'Corporate website for the company “TOP Security KZ” (Security company)',
+      textAltynBulak: 'Website for the residential complex in the city of Almaty',
+      textTSTrack2: 'Redesign of a mobile panic button site',
+      textZhagalau: 'Website for the residential complex in the city of Almaty',
+      textKausar: 'Website for the residential complex in the city of Almaty',
+      textPestHunter: 'Website for a pest control and disinfection company',
+      textCleaningMaster: 'Website for a cleaning company',
+      textMuratovMarket: 'Online store for a social store',
+      textMuratovStamp: 'Website for a printing company',
+      textIrishCoffee: 'Website business card for coffee network in the city of Almaty',
+      textLadySecrets: 'Landing for an exclusive female beauty studio',
+      textNedvijka: 'Website for a real estate company',
+      textPMG: 'Website for an investment company',
+      textTSApp: 'Mobile app, panic button ‘’ TOP Security ’’',
+      textSpeedReadApp: 'Educational game, speed reading, on iOS and Android',
+      textBoomBrains: 'Educational game on iOS and Android'
+    },
+
+    brif: {
+      title: 'Make a brief '
+    },
+
+    contact: {
+      contact: 'Contacts',
+      address: 'Address',
+      social: 'Social media',
+      addressCountry: 'Kazakhstan, Almaty, ',
+      addressStreet: 'Shevchenko str 153/1'
+    }
+  },
+
+  ru: {   
+
+    header: {
+      title1: 'UI/UX Дизайн',
+      title2: 'Разработка Web-сервисов',
+      title3: 'Разработка мобильных игр и приложений'
+    },
+
+    menu: {
+      all: 'Все проекты',
+      game: 'Игры',
+      app: 'Приложения',
+      service: 'Веб-сервисы',
+      site: 'Сайты'
+    },
+
+    work: {
+      textMurPart: 'Корпоративный сайт для юридической компании',
+      textbbh: 'Сайт для жилого комплекса таунхаусов в горах города Алматы',
+      textProfRem: 'Сайт для центра профессионального ремонта',
+      textProfLegal: 'Сайт для палаты юридических консультантов',
+      textTSTrack: 'Сайт мобильной тревожной кнопки',
+      textTS: 'Сайт для компании предоставляющей услуги охраны',
+      textTopSignal: 'Лендинг для приложения мобильная кнопка',
+      textNewEstate: 'Корпоративный сайт строительной компании',
+      textTSKZ: 'Корпоративный сайт для компании “TOP Security KZ” (Охранная компания)',
+      textAltynBulak: 'Сайт для Жилого комплекса в городе Алматы',
+      textTSTrack2: 'Редизайн сайта мобильной тревожной кнопки',
+      textZhagalau: 'Сайт для Жилого комплекса в городе Алматы',
+      textKausar: 'Сайт для Жилого комплекса в городе Алматы',
+      textPestHunter: 'Сайт для компании по дизенсекции и борьбы с вредителями',
+      textCleaningMaster: 'Сайт для компании по дизенсекции и борьбы с вредителями',
+      textMuratovMarket: 'Интернет магазин для социального магазина',
+      textMuratovStamp: 'Сайт для компании изаготавливающей печати',
+      textIrishCoffee: 'Сайт визитка для сети коффейнь в городе Алматы',
+      textLadySecrets: 'Лендинг для эксклюзивной женской студии красоты',
+      textNedvijka: 'Сайт для риелторской компании',
+      textPMG: 'Сайт для инвестиционной компании',
+      textTSApp: 'Мобильное приложение, тревожная кнопка ‘’TOP Security’’',
+      textSpeedReadApp: 'Развивающая игра, скорочтение, на IOS и Android.',
+      textBoomBrains: 'Развивающая игра, на IOS и Android.'
+    },
+
+    brif: {
+      title: 'Заполнить бриф '
+    },
+
+    contact: {
+      contact: 'Контакты',
+      address: 'Адрес',
+      social: 'Соцсети',
+      addressCountry: 'Казахстан, г. Алматы, ',
+      addressStreet: 'ул. Шевченко 153/1'
+    }
+  },  
+
+  kz: {
+
+  }
+}
